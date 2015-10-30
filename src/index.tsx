3da2cb90b5b9fb39613f15ebf342b05a0de488d0
Joseph McElroy <phoey1@gmail.com>
2015-10-30 08:37:46 +0000

    fix path issue

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import App from "./components/app/src/App.tsx";
+import Site from "./components/site/src/App.tsx";
 import Home from "./components/home/src/Home.tsx";
 import OpentableCaseStudy from "./components/opentable-casestudy/src/OpentableCaseStudy.tsx";
 import {Router, Route, IndexRoute} from "react-router";
 
 ReactDOM.render((
   <Router>
-    <Route path="/" component={App}>
+    <Route path="/" component={Site}>
       <IndexRoute component={Home}/>
       <Route path="case-studies">
         <Route path="opentable" component={OpentableCaseStudy}/>
